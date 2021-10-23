940cff788048b700ec1354a447b806f9b54aa89d
neko37 <37131716@qq.com>
2021-10-23 17:02:40 +0800

    Update Catelog.vue

--- src/views/Catelog.vue
+++ src/views/Catelog.vue
@@ -21,15 +21,150 @@
         <van-sidebar-item title="文具" />
         <van-sidebar-item title="积分专区" />
       </van-sidebar>
-      <div style="width: 78%">
-        <van-grid :column-num="3">
-          <van-grid-item
-            v-for="item in lists"
-            :key="item.id"
-            :icon="item.image"
-            :text="item.name"
-          />
-        </van-grid>
+      <div style="width: 78%; float: right">
+        <div>
+          <div
+            style="
+              border-left: 6px solid blue;
+              border-bottom: 2px solid gray;
+              margin-top: 5%;
+              padding: 5%;
+            "
+          >
+            <h1>零食点心</h1>
+          </div>
+          <div class="shopl">
+            <van-grid :column-num="3">
+              <van-grid-item
+                v-for="item in lists"
+                :key="item.id"
+                :icon="item.image"
+                :text="item.name"
+              />
+            </van-grid>
+          </div>
+        </div>
+
+        <div
+          style="
+            border-left: 6px solid red;
+            border-bottom: 2px solid gray;
+            margin-top: 5%;
+            padding: 5%;
+          "
+        >
+          <h1>酒水</h1>
+        </div>
+        <div class="shopl">
+          <van-grid :column-num="3">
+            <van-grid-item
+              v-for="item in lists"
+              :key="item.id"
+              :icon="item.image"
+              :text="item.name"
+            />
+          </van-grid>
+        </div>
+        <div
+          style="
+            border-left: 6px solid green;
+            border-bottom: 2px solid gray;
+            margin-top: 5%;
+            padding: 5%;
+          "
+        >
+          <h1>新鲜水果</h1>
+        </div>
+        <div class="shopl">
+          <van-grid :column-num="3">
+            <van-grid-item
+              v-for="item in lists"
+              :key="item.id"
+              :icon="item.image"
+              :text="item.name"
+            />
+          </van-grid>
+        </div>
+        <div
+          style="
+            border-left: 6px solid yellow;
+            border-bottom: 2px solid gray;
+            margin-top: 5%;
+            padding: 5%;
+          "
+        >
+          <h1>生活用品</h1>
+        </div>
+        <div class="shopl">
+          <van-grid :column-num="3">
+            <van-grid-item
+              v-for="item in lists"
+              :key="item.id"
+              :icon="item.image"
+              :text="item.name"
+            />
+          </van-grid>
+        </div>
+        <div
+          style="
+            border-left: 6px solid orange;
+            border-bottom: 2px solid gray;
+            margin-top: 5%;
+            padding: 5%;
+          "
+        >
+          <h1>体育用品</h1>
+        </div>
+        <div class="shopl">
+          <van-grid :column-num="3">
+            <van-grid-item
+              v-for="item in lists"
+              :key="item.id"
+              :icon="item.image"
+              :text="item.name"
+            />
+          </van-grid>
+        </div>
+        <div
+          style="
+            border-left: 6px solid pink;
+            border-bottom: 2px solid gray;
+            margin-top: 5%;
+            padding: 5%;
+          "
+        >
+          <h1>文具</h1>
+        </div>
+        <div class="shopl">
+          <van-grid :column-num="3">
+            <van-grid-item
+              v-for="item in lists"
+              :key="item.id"
+              :icon="item.image"
+              :text="item.name"
+            />
+          </van-grid>
+        </div>
+        <div
+          style="
+            border-left: 6px solid gold;
+            border-bottom: 2px solid gray;
+            margin-top: 5%;
+            padding: 5%;
+          "
+        >
+          <h1>积分专区</h1>
+        </div>
+        <div class="shopl">
+          <van-grid :column-num="3">
+            <van-grid-item
+              v-for="item in lists"
+              :key="item.id"
+              :icon="item.image"
+              :text="item.name"
+            />
+          </van-grid>
+        </div>
       </div>
     </div>
     <navb></navb>
