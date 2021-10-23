e83b76358da008e1ef1685b92de9ba945730e594
neko37 <37131716@qq.com>
2021-10-23 22:34:15 +0800

    0

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,33 @@
 <template>
-  <div class="about">
+  <div class="headerabout">
+    <van-nav-bar title="关于我们" left-arrow />
+    <van-cell-group class="gscx">
+      <van-cell title="公司简介" is-link url="/vant/mobile.html" />
+      <van-cell title="门店查询" is-link url="/vant/mobile.html" />
+      <van-cell title="联系我们" class="bab" is-link url="/vant/mobile.html" />
+    </van-cell-group>
+  
+    <foot></foot>
   </div>
 </template>
+
+<script>
+import Foot from "../components/Foot.vue"
+export default { components:{
+Foot
+}};
+</script>
+
+<style lang="less" scoped>
+.headerabout{
+  background: white;
+}
+.bab {
+  min-height: 10rem;
+}
+.txt > p {
+  text-align: center;
+  line-height: 1.3rem;
+  color: darkgrey;
+}
+</style>>
