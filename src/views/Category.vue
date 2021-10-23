7c8ed797f8018f8dc3126ccd3e515230da195acc
neko37 <37131716@qq.com>
2021-10-23 21:21:37 +0800

    Update Category.vue

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,5 +1,21 @@
 <template>
-  <navb></navb>
+  <div>
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        action-text="筛选"
+        shape="round"
+        maxlength="12"
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+      />
+    </form>
+    <div>
+        
+    </div>
+    <navb></navb>
+  </div>
 </template>
 
 <script>
