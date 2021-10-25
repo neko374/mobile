44e88e587572243924c67c8993fd860350ab83f3
dyw99 <2573874956@qq.com>
2021-10-25 16:02:21 +0800

    6

--- src/router/index.js
+++ src/router/index.js
@@ -83,6 +83,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/pocking.vue')
   },
   {
+    path: '/dizhi',
+    name: 'dizhi',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Flow_dizhi.vue')
+  },
+  {
     path: '/userbackorder',
     name: 'UserBackOrder',
     component: () => import('../views/UserBackOrder.vue')
