763e53c208d5ef0d31f7768950e36ade160d108a
neko37 <37131716@qq.com>
2021-10-25 16:12:46 +0800

    Update navb.vue

--- src/components/navb.vue
+++ src/components/navb.vue
@@ -3,7 +3,7 @@
     <div style="height: 3rem"></div>
     <van-tabbar v-model="active">
       <van-tabbar-item icon="home-o"
-        ><router-link to="/home">首页</router-link></van-tabbar-item
+        ><router-link to="/">首页</router-link></van-tabbar-item
       >
       <van-tabbar-item icon="apps-o"
         ><router-link to="/catelog">分类</router-link></van-tabbar-item
