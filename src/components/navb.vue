dbf448a482eafd355b703508b2efcdfb6a743069
neko37 <37131716@qq.com>
2021-10-25 16:09:25 +0800

    Update navb.vue

--- src/components/navb.vue
+++ src/components/navb.vue
@@ -2,11 +2,15 @@
   <div>
     <div style="height: 3rem"></div>
     <van-tabbar v-model="active">
-      <van-tabbar-item icon="home-o"><router-link to="/home">首页</router-link></van-tabbar-item>
+      <van-tabbar-item icon="home-o"
+        ><router-link to="/home">首页</router-link></van-tabbar-item
+      >
       <van-tabbar-item icon="apps-o"
         ><router-link to="/catelog">分类</router-link></van-tabbar-item
       >
-      <van-tabbar-item icon="cart-o" badge="5"><router-link to="/flow">购物车</router-link></van-tabbar-item>
+      <van-tabbar-item icon="cart-o" badge="5"
+        ><router-link to="/flow">购物车</router-link></van-tabbar-item
+      >
       <van-tabbar-item icon="friends-o" dot>我的</van-tabbar-item>
     </van-tabbar>
   </div>
